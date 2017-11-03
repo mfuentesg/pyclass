243fe5e832b7c1a90ddf622cb640ded00a54b14d
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2017-11-03 09:35:10 -0300

    master - solve wrong cases

--- kangaroo/kangaroo.py
+++ kangaroo/kangaroo.py
@@ -7,8 +7,8 @@ def kangaroo(k1, k2):
     x1 + j * v1 = x2 + j * v2 -> where j is the jumps number
 
     Constraints:
-        -  v1 > v2
-        - j must be a positive number
+        - j > 0
+        - v1 > v2
 
     Solving the equation
         x1 + j * v1 = x2 + j * v2
@@ -21,11 +21,7 @@ def kangaroo(k1, k2):
     x1, v1 = k1
     x2, v2 = k2
 
-    # if this condition is true, the kangaroos never will meet
-    if v1 <= v2:
-        return False
-
     # calculating the jumps number
-    j = abs(x1 - x2) // abs(v1 - v2)
+    j = abs(x1 - x2) // abs(v1 - v2) if abs(v1 - v2) > 0 else 1
 
     return (x1 + v1 * j) == (x2 + v2 * j)
--- kangaroo/kangaroo_test.py
+++ kangaroo/kangaroo_test.py
@@ -7,9 +7,7 @@ def test(base, expected):
 
 
 test(kangaroo((0, 3), (4, 2)), True)
-test(kangaroo((0, 3), (4, 2)), True)
-test(kangaroo((0, 3), (4, 2)), True)
-test(kangaroo((0, 3), (4, 2)), True)
-test(kangaroo((0, 3), (4, 2)), True)
-test(kangaroo((0, 3), (4, 2)), True)
-test(kangaroo((0, 3), (4, 2)), True)
+test(kangaroo((3, 3), (2, 2)), False)
+test(kangaroo((0, 3), (0, 3)), True)
+test(kangaroo((35, 1), (45, 3)), False)
+test(kangaroo((3585, 7317), (6994, 9610)), False)
